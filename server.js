b53347ac3537eddd3656aea1a5aec87bc74d31b9
AwhAlpha <46508035+AwhAlpha@users.noreply.github.com>
2020-10-26 16:55:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const ScriptServer = require('scriptserver');
 
 const server = new ScriptServer({
   core: {
-    jar: 'minecraft_server.jar',
+    jar: 'minecraft_server.ar',
     args: ['-Xmx512M'],
       rcon: {
       port: '25575',
